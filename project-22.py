bd3eb0aaba8d38f6cd24a0af8f4ed3aceab4d1b8
Suphal <suphalbochkar@gmail.com>
2022-11-26 00:45:12 +0530

    Input's

---
+++ project-22.py
@@ -0,0 +1,7 @@
+print()
+names = ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
+marks = [53,43,31,39,28,21,38,25,15,19]
+updates = [-5,-22,-9,-7,+9,-19,-7,-3,-6,+3]
+a={}
+um=[]
+ua={}
